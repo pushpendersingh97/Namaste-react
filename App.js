23286e08da515dd1cf1b4c677a245d357460630e
Pushpender Singh <pushpender.singh@geminisolutions.com>
2023-09-10 18:14:16 +0530

    Nested React createElement

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1></h1>
+ *      <h2></h2>
+ *  </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]
+  )
+);
+
+// React object
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//   },
+//   "Hello World from React"
+// );
+
+console.log(parent); //Object
+
+// Get Root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
