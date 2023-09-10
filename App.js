6e1da26d2be40f68ccbf3bfbf0c46aa1a3568327
Pushpender Singh <pushpender.singh@geminisolutions.com>
2023-09-10 18:16:58 +0530

    Part 4: React.createElement nested elemenets

--- App.js
+++ App.js
@@ -12,16 +12,28 @@ const parent = React.createElement(
   {
     id: "parent",
   },
-  React.createElement(
-    "div",
-    {
-      id: "child",
-    },
-    [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]
-  )
+  [
+    React.createElement(
+      "div",
+      {
+        id: "child1",
+      },
+      [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+      ]
+    ),
+    React.createElement(
+      "div",
+      {
+        id: "child2",
+      },
+      [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+      ]
+    ),
+  ]
 );
 
 // React object
