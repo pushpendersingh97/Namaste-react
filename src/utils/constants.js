cef86a73cb1ebd7a8b89f80be719655c03b35d02
Pushpender Singh <Pushpender.singh@geminisolutions.in>
2023-10-18 18:37:07 +0530

    Header UI revamp done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTThk6B4F2Q-aQEosit-VHmYfxhiDDAlBNo-JuA8nQN&s";
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4594965&lng=77.0266383&restaurantId="
 export const GITHUB_URL = "https://api.github.com/users/"
